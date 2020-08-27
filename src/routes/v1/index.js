2d9fef7545c6c2ed49b1f24e3b702a9ca8669b9a
enrique garcia <enriquegarcia259@yahoo.es>
2020-08-26 19:00:56 -0600

    Guardando un producto en MongoDB

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,5 +5,5 @@ const usersRoutes = require('./users-routes')
 //funcion que sera exportada y recibe como parametro una aplicacion de express
 module.exports = app =>{
      app.use('/api/v1/users', usersRoutes)
-     //app.use('/api/v1/products', productsRoutes)
+     app.use('/api/v1/products', productsRoutes)
 }
--- src/routes/v1/products-routes.js
+++ src/routes/v1/products-routes.js
@@ -1,7 +1,13 @@
 /**
  * defino todas las rutas que se van a comunicar con el controlador product.js
  */
-
 const express = require('express')
-
 const productController = require('../../controllers/v1/products-controller')
+const router = express.Router()
+
+
+router.post('/create', productController.createProduct)
+
+
+
+module.exports = router
